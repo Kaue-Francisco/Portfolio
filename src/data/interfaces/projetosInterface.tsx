bbd48b09dd44c8a8c24967d1097772bce93e8c74
Kaue-Francisco <kauesantos.francisco@gmail.com>
2024-12-02 07:53:36 -0300

    feat: Adicionando sessão de softskills.

--- src/data/interfaces/projetosInterface.tsx
+++ src/data/interfaces/projetosInterface.tsx
@@ -3,6 +3,7 @@ export interface Project {
     image: string;
     category: string;
     details: string;
+    softskills: string;
     technologies: string[];
     link: string;
   }
--- src/data/projetos/projetosData.tsx
+++ src/data/projetos/projetosData.tsx
@@ -11,6 +11,7 @@ export const projects: Project[] = [
     image: 'https://github.com/Kaue-Francisco/Deepsort_Yolov5/raw/main/media/example.gif',
     category: 'Pessoal',
     details: "Este projeto contém um código simples de rastreamento em objetos (Deep SORT). O código carrega o modelo coco yolov5 e o utiliza para detectar carros na estrada, conforme mostrado no exemplo.",
+    softskills: "",
     technologies: ['Python', 'JupyterNotebook', 'OpenCV', 'YOLOv5'], // Exemplo de tecnologias utilizadas
     link: 'https://github.com/Kaue-Francisco/Deepsort_Yolov5.git'
   },
@@ -18,24 +19,27 @@ export const projects: Project[] = [
     title: 'API 3° Semestre',
     image: API3.src,
     category: 'FATEC Prof. Jessen Vidal',
-    details: "Este projeto é uma aplicação de portal de transparência para a fundação FAPG. Seu objetivo era realizar uma aplicação intuitiva que atingesse todos os requisistos de administração do cliente. No projeto eu colbarei com o envio de email para o administrador quando o projeto for vencer e refatoração do código. SoftSkills: Neste projeto atuei novamente como Scrum Master mas de uma forma mais correta, novamente fiz um grupo com pessoas diferentes na intenção de melhorar minha comunicação e trabalho em equipe.",
-    technologies: ['Spring Boot', 'Java', 'React', 'MySQL'], // Exemplo de tecnologias utilizadas
+    details: "Este projeto é uma aplicação de portal de transparência para a fundação FAPG. Seu objetivo era realizar uma aplicação intuitiva que atingesse todos os requisistos de administração do cliente. No projeto eu colabarei com o envio de email para o administrador quando o projeto for vencer e refatoração do código.",
+    softskills: "SoftSkills: Neste projeto atuei como Scrum Master mas de uma forma mais correta, novamente fiz um grupo com pessoas diferentes na intenção de melhorar minha comunicação e trabalho em equipe. Dificuldades que encontrei nesse projeto foi que ao longo do desenvolvimento do projeto teve novos integrantes no grupo e um grupo que era formado por 6 pessoas virou 9 pessoas, então teve um processo de adaptação.",
+    technologies: ['Spring Boot', 'Java', 'React', 'MySQL'],
     link: 'https://github.com/Sync-FATEC/API-2024.2-3SEM'
   },
   {
     title: 'API 2° Semestre',
     image: API2.src,
     category: 'FATEC Prof. Jessen Vidal',
-    details: 'O projeto é uma aplicação web desenvolvida para facilitar a gestão de solicitações de suporte técnico. Neste projeto eu colaborei com o desenvolvimento backend da aplicação. Realizei funcionalidade como criação de Problema, validação de horário de técnico, organização de código. SoftSkills: neste projeto eu me juntei ao novo grupo de pessoas na intenção de melhorar o trabalhar em equipe. Neste projeto atuei como desenvolvedor.',
-    technologies: ['React', 'Prisma', 'Node.js', 'TypeScript', 'MySQL'], // Exemplo de tecnologias utilizadas
+    details: 'O projeto é uma aplicação web desenvolvida para facilitar a gestão de solicitações de suporte técnico. Neste projeto eu colaborei com o desenvolvimento backend da aplicação. Realizei funcionalidade como criação de Problema, validação de horário de técnico, organização de código.',
+    softskills: 'SoftSkills: Neste projeto atuei como Desenvolvedor para aprimorar minha autonomia, comuncação e proatividadade eu me juntei ao novo grupo de pessoas na intenção de melhorar o trabalhar em equipe. Dificuldades que foi encontrei nesse projeto foi que o grupo não era totalmente comunicativo era separado em 2 polos o que prejudicava a comunicação.',
+    technologies: ['React', 'Prisma', 'Node.js', 'TypeScript', 'MySQL'],
     link: 'https://github.com/TeamHiveAPI/API-2023.2'
   },
   {
     title: 'API 1° Semestre',
     image: API1.src,
     category: 'FATEC Prof. Jessen Vidal',
-    details: 'Este projeto teve o intuito de fazer uma plataforma de auxilio para mães que possuem filhos com Insuficiencia Renal Cronica. Neste projeto eu colaborei com o desenvolvimento backend da aplicação. Realizei funcinalidades como, troca de senha, validação de usuario, criar conta, criar postagem do blog. SoftSkills: este projeto foi realizado no primeiro semeste da minha faculdade com isso a dificuldade que encontrei foi a adaptação ao trabalho em equipe, a comunicação e a organização de tarefas. Neste projeto atuei como Scrum Master.',
-    technologies: ['Python', 'JavaScript', 'Flask', 'HTML', 'CSS', 'MySQL'], // Exemplo de tecnologias utilizadas
+    details: 'Este projeto teve o intuito de fazer uma plataforma de auxilio para mães que possuem filhos com Insuficiencia Renal Cronica. Neste projeto eu colaborei com o desenvolvimento backend da aplicação. Realizei funcinalidades como, troca de senha, validação de usuario, criar conta, criar postagem do blog.',
+    softskills: 'SoftSkills: Neste projeto atuei como Scrum Master foi realizado no primeiro semeste da minha faculdade com isso eu tive minha primeira experiência em trabalho em equipe com pessoas que não conhecia, comunicação e a organização de tarefas. Dificuldades que encontrei nesse projeto foi que os integrantes não estavam muito interessantes no desenvolvimento do projeto, então teve pessoas com sobrecarga de tarefas e outras pessoas acabaram ficando sem fazer nada.',
+    technologies: ['Python', 'JavaScript', 'Flask', 'HTML', 'CSS', 'MySQL'],
     link: 'https://github.com/Kaue-Francisco/Pixels'
   },
   {
@@ -43,7 +47,8 @@ export const projects: Project[] = [
     image: Barbearia.src,
     category: 'Pessoal',
     details: 'É um sistema de agendamento para a Barbearia Companhia do Bigode.',
-    technologies: ['React', 'TypeScript', 'Prisma', 'Node.js', 'MySQL'], // Exemplo de tecnologias utilizadas
+    softskills: '',
+    technologies: ['React', 'TypeScript', 'Prisma', 'Node.js', 'MySQL'],
     link: 'https://github.com/Kaue-Francisco/Barbearia'
   },
   {
@@ -51,7 +56,8 @@ export const projects: Project[] = [
     image: 'https://github.com/Kaue-Francisco/ClimateCheck/raw/main/static/demonstracao/home.png',
     category: 'Pessoal',
     details: 'Um projeto aonde você consegue ver o clima de uma cidade específica.',
-    technologies: ['Python', 'Flask', 'HTML', 'CSS', 'WeatherAPI'], // Exemplo de tecnologias utilizadas
+    softskills: '',
+    technologies: ['Python', 'Flask', 'HTML', 'CSS', 'WeatherAPI'],
     link: 'https://github.com/Kaue-Francisco/ClimateCheck'
   },
   {
@@ -59,7 +65,8 @@ export const projects: Project[] = [
     image: imageVendas.src,
     category: 'FATEC Prof. Jessen Vidal',
     details: 'É o Projeto Final Desenvolvimento Web na FATEC. Gerenciamento de Vendas de um mercado aonde o administrador consegue, adicionar, editar, excluir e visualizar produtos, vendas e clientes',
-    technologies: ['React', 'Prisma', 'TypeScript', 'Node.js', 'Express', 'MySQL'], // Exemplo de tecnologias utilizadas
+    softskills: '',
+    technologies: ['React', 'Prisma', 'TypeScript', 'Node.js', 'Express', 'MySQL'],
     link: 'https://github.com/Kaue-Francisco/Desenvolvimento-Web-2/tree/main/projeto_final',
   },
 ];
